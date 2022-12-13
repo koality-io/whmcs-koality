1d562c35a7dd4272790cc1264e14bc0bfe10f3c1
Malte Schlüter <malte.schlueter@webpros.com>
2022-12-13 14:04:43 +0100

    Move hook business code to own service class

--- hooks.php
+++ hooks.php
@@ -8,9 +8,9 @@ declare(strict_types=1);
 
 use Illuminate\Database\Capsule\Manager as Capsule;
 use WHMCS\Module\Server\Koality\Constants;
-use WHMCS\Module\Server\Koality\CustomFields;
 use WHMCS\Module\Server\Koality\Exception\CouldNotCheckAndAddCustomFields;
 use WHMCS\Module\Server\Koality\Logger;
+use WHMCS\Module\Server\Koality\Service\AddRequiredCustomFields;
 
 require __DIR__ . '/vendor/autoload.php';
 
@@ -45,78 +45,9 @@ add_hook('ProductEdit', 1, static function ($vars) {
         return [];
     }
 
-    $customFields = Capsule::table('tblcustomfields')
-        ->where('relid', '=', $pid)
-        ->get()
-    ;
+    $addRequiredCustomFields = new AddRequiredCustomFields(Capsule::connection());
 
-    $hasFieldKeyId = false;
-    $hasFieldActivationCode = false;
-    $hasFieldActivationUrl = false;
-
-    foreach ($customFields as $customField) {
-        if ($customField->fieldname === CustomFields::KEY_ID) {
-            $hasFieldKeyId = true;
-
-            continue;
-        }
-
-        if ($customField->fieldname === CustomFields::ACTIVATION_CODE) {
-            $hasFieldActivationCode = true;
-
-            continue;
-        }
-
-        if ($customField->fieldname === CustomFields::ACTIVATION_URL) {
-            $hasFieldActivationUrl = true;
-
-            continue;
-        }
-    }
-
-    $nowDate = date('Y-m-d H:i:s');
-
-    if (!$hasFieldKeyId) {
-        Capsule::table('tblcustomfields')
-            ->insert([
-                'type' => 'product',
-                'relid' => $pid,
-                'fieldname' => CustomFields::KEY_ID,
-                'fieldtype' => 'text',
-                'adminonly' => 'on',
-                'created_at' => $nowDate,
-                'updated_at' => $nowDate,
-            ])
-        ;
-    }
-
-    if (!$hasFieldActivationCode) {
-        Capsule::table('tblcustomfields')
-            ->insert([
-                'type' => 'product',
-                'relid' => $pid,
-                'fieldname' => CustomFields::ACTIVATION_CODE,
-                'fieldtype' => 'text',
-                'adminonly' => 'on',
-                'created_at' => $nowDate,
-                'updated_at' => $nowDate,
-            ])
-        ;
-    }
-
-    if (!$hasFieldActivationUrl) {
-        Capsule::table('tblcustomfields')
-            ->insert([
-                'type' => 'product',
-                'relid' => $pid,
-                'fieldname' => CustomFields::ACTIVATION_URL,
-                'fieldtype' => 'text',
-                'adminonly' => 'on',
-                'created_at' => $nowDate,
-                'updated_at' => $nowDate,
-            ])
-        ;
-    }
+    ($addRequiredCustomFields)($pid);
 
     return [];
 });
---
+++ src/Service/AddRequiredCustomFields.php
@@ -0,0 +1,82 @@
+<?php
+/*
+ * Copyright 2022. Plesk International GmbH. All rights reserved.
+ */
+
+declare(strict_types=1);
+
+namespace WHMCS\Module\Server\Koality\Service;
+
+use Illuminate\Database\ConnectionInterface;
+use WHMCS\Module\Server\Koality\CustomFields;
+
+final class AddRequiredCustomFields
+{
+    private ConnectionInterface $databaseConnection;
+
+    public function __construct(ConnectionInterface $databaseConnection)
+    {
+        $this->databaseConnection = $databaseConnection;
+    }
+
+    public function __invoke(int $pid): void
+    {
+        $customFields = $this->databaseConnection->table('tblcustomfields')
+            ->where('relid', '=', $pid)
+            ->get()
+        ;
+
+        $hasFieldKeyId = false;
+        $hasFieldActivationCode = false;
+        $hasFieldActivationUrl = false;
+
+        $nowDate = date('Y-m-d H:i:s');
+
+        foreach ($customFields as $customField) {
+            if ($customField->fieldname === CustomFields::KEY_ID) {
+                $hasFieldKeyId = true;
+
+                continue;
+            }
+
+            if ($customField->fieldname === CustomFields::ACTIVATION_CODE) {
+                $hasFieldActivationCode = true;
+
+                continue;
+            }
+
+            if ($customField->fieldname === CustomFields::ACTIVATION_URL) {
+                $hasFieldActivationUrl = true;
+
+                continue;
+            }
+        }
+
+        if (!$hasFieldKeyId) {
+            $this->createAndSaveCustomField($pid, CustomFields::KEY_ID, $nowDate);
+        }
+
+        if (!$hasFieldActivationCode) {
+            $this->createAndSaveCustomField($pid, CustomFields::ACTIVATION_CODE, $nowDate);
+        }
+
+        if (!$hasFieldActivationUrl) {
+            $this->createAndSaveCustomField($pid, CustomFields::ACTIVATION_URL, $nowDate);
+        }
+    }
+
+    private function createAndSaveCustomField(int $pid, string $fieldName, string $nowDate): void
+    {
+        $this->databaseConnection->table('tblcustomfields')
+            ->insert([
+                'type' => 'product',
+                'relid' => $pid,
+                'fieldname' => $fieldName,
+                'fieldtype' => 'text',
+                'adminonly' => 'on',
+                'created_at' => $nowDate,
+                'updated_at' => $nowDate,
+            ])
+        ;
+    }
+}
